fee019cbcb36787b68e946008fdaa0b4bcfcdeb0
Abeer Gandhi <abeergandhi2@gmail.com>
2026-06-02 22:29:18 +0530

    fix: restore closing /> on AuthModal tag

--- app/page.tsx
+++ app/page.tsx
@@ -123,7 +123,7 @@ export default function LandingPage() {
                 isOpen={isAuthModalOpen}
                 onClose={() => setIsAuthModalOpen(false)}
                 defaultMode="login"
-            /
+            />
         </div>
     );
 }
